2a7d9340a5320196a323195492f00a10b090f26b
William C Grisaitis <william@grisait.is>
2019-09-23 18:24:44 -0400

    Ignore target/ and other rust stuff

--- 02_hello_cargo/hello_cargo/target/debug/deps/hello_cargo-973ac8d582b53d73.d
+++
@@ -1,5 +0,0 @@
-/Users/william/src/rust/02_hello_cargo/hello_cargo/target/debug/deps/hello_cargo-973ac8d582b53d73: src/main.rs
-
-/Users/william/src/rust/02_hello_cargo/hello_cargo/target/debug/deps/hello_cargo-973ac8d582b53d73.d: src/main.rs
-
-src/main.rs:
--- 02_hello_cargo/hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/Users/william/src/rust/02_hello_cargo/hello_cargo/target/debug/hello_cargo.dSYM: /Users/william/src/rust/02_hello_cargo/hello_cargo/src/main.rs
