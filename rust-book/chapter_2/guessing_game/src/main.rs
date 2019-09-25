285d95b106dea3fbbd60e6adc5981a7649993347
William C Grisaitis <william@grisait.is>
2019-09-25 16:48:36 -0400

    Edit main.rs

--- rust-book/chapter_2/guessing_game/src/main.rs
+++ rust-book/chapter_2/guessing_game/src/main.rs
@@ -4,10 +4,10 @@ fn main() {
     println!("guess the number!");
     println!("input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); // growable, utf-8
 
     io::stdin().read_line(&mut guess)
         .expect("failed to read line.");
     
-    println!("you guessed {}", guess)
+    println!("you guessed {}", guess);
 }
