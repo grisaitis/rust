cd4675cc1f54568c3c32cd04fd197eb2c6a29375
William C Grisaitis <william@grisait.is>
2019-09-30 11:31:42 -0400

    Format other guessing game

--- rust-book/chapter_2/guessing_game_from_memory/src/main.rs
+++ rust-book/chapter_2/guessing_game_from_memory/src/main.rs
@@ -1,20 +1,20 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
-
+use std::io;
 
 fn main() {
     let secret: u32 = rand::thread_rng().gen_range(1, 101);
     loop {
         let mut guess = String::new();
         println!("input guess:");
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("Failure to read from stdin");
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(message) => {
                 println!("Error: {}", message);
-                continue
+                continue;
             }
         };
         match guess.cmp(&secret) {
@@ -22,7 +22,7 @@ fn main() {
             Ordering::Greater => println!("Too high"),
             Ordering::Equal => {
                 println!("Success!");
-                break
+                break;
             }
         }
     }
