5eaf4d8a46e07f701bef512df9ab707ab496b497
William C Grisaitis <william@grisait.is>
2019-09-30 15:11:32 -0400

    Add functions to demo integer overflow and shadow variables

--- rust-book/chapter_3/variables/src/main.rs
+++ rust-book/chapter_3/variables/src/main.rs
@@ -3,8 +3,29 @@ fn main() {
     print_variable(x);
     let x = x * 2.0;
     print_variable(x);
+    // overflow_wrap_around_demo();
+    shadow_scope_demo();
 }
 
 fn print_variable(v: f64) {
     println!("{}", v);
+}
+
+fn shadow_scope_demo() {
+    let y: u8 = 0;
+    loop {
+        // y never increments
+        // this let statement is in a new scope
+        let y = y + 1;
+        println!("{}", y);
+    }
+}
+
+fn overflow_wrap_around_demo() {
+    let mut y: u8 = 0;
+    loop {
+        // this y does increment
+        y += 1;
+        println!("{}", y);
+    }
 }
