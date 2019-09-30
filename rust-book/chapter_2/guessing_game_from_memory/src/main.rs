6960baed557320a219d06e593b7a019f72532bef
William C Grisaitis <william@grisait.is>
2019-09-30 11:31:32 -0400

    Add guessing game implemented from memory

---
+++ rust-book/chapter_2/guessing_game_from_memory/src/main.rs
@@ -0,0 +1,30 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+
+fn main() {
+    let secret: u32 = rand::thread_rng().gen_range(1, 101);
+    loop {
+        let mut guess = String::new();
+        println!("input guess:");
+        io::stdin().read_line(&mut guess)
+            .expect("Failure to read from stdin");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(message) => {
+                println!("Error: {}", message);
+                continue
+            }
+        };
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too low"),
+            Ordering::Greater => println!("Too high"),
+            Ordering::Equal => {
+                println!("Success!");
+                break
+            }
+        }
+    }
+    println!("Goodbye, world!");
+}
