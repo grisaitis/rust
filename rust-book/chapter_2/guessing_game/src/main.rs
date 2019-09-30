18ade8bb66fd0f2f2a3d4d61c43e5e49d8228307
William C Grisaitis <william@grisait.is>
2019-09-30 10:30:37 -0400

    Implement guessing game, with loop, pattern matching, ordering, etc.

--- rust-book/chapter_2/guessing_game/src/main.rs
+++ rust-book/chapter_2/guessing_game/src/main.rs
@@ -1,13 +1,25 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("guess the number!");
-    println!("input your guess.");
-
-    let mut guess = String::new(); // growable, utf-8
-
-    io::stdin().read_line(&mut guess)
-        .expect("failed to read line.");
-    
-    println!("you guessed {}", guess);
+    let secret: u32 = rand::thread_rng().gen_range(1, 101);
+    loop {
+        println!("input your guess...");
+        let mut guess = String::new(); // growable, utf-8
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line.");
+        let guess: u32 = guess.trim().parse().expect("Failed to parse");
+        println!("you guessed {}.", guess);
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Too low"),
+            Ordering::Greater => println!("Too high"),
+            Ordering::Equal => {
+                println!("Success!");
+                break;
+            }
+        }
+    }
 }
